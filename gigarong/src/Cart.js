6e0e0fbe660557e61896bad987e0d507acfc6b21
poiupoiu96 <poiupoiu96@naver.com>
2021-12-18 23:38:01 +0900

    redux (reducer, dispatch)

--- gigarong/src/Cart.js
+++ gigarong/src/Cart.js
@@ -2,35 +2,63 @@
 
 import React from 'react';
 import { Table} from 'react-bootstrap'
+import { connect } from 'react-redux';
 
 // 카트 
-function Cart () {
+function Cart (props) {
     return (
         <Table responsive>
             <thead>
                 <tr>
                     <th>#</th>
+                    <th>상품컬러</th>
                     <th>상품명</th>
-                    <th>수량</th>
+                    <th>수량</th> 
                     <th>변경</th> 
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td> 
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td> 
-                </tr>
+                {
+                    props.state.map((item, idx) => {
+                        return (
+                            <tr key={idx}>
+                                <td>{idx}</td>
+                                <td>{ item.id }</td>
+                                <td>{ item.name }{idx}</td>
+                                <td>{ item.quan }</td>
+                                <td> <button onClick={()=>{ props.dispatch({type: 'quanAdd'}) }}> + </button> &nbsp;
+                                <button onClick={()=>{ props.dispatch({type: 'quanMin'}) }}> - </button></td>
+                            </tr>
+                        )
+                    })
+                }
             </tbody>
         </Table>
     )
 }
 
-export default Cart;
+// redux store 데이터 가져와서 props로 변환해주는 함수
+// 함수 만들고 export default connect(함수명)(return)
+function getCreateStore(state) {
+    return {
+        // name: state[0].name
+        state: state
+    }
+}
+
+// function TableDiv (param,idx) { 
+//     console.log(param)
+//     return (
+//         <tr key={param.idx}>
+//             <td>{param.idx}</td>
+//             <td>{ param.param.id }</td>
+//             <td>{ param.param.name }{param.idx}</td>
+//             <td>{ param.param.quan }</td>
+//             <td> <button onClick={()=>{ param.param.dispatch({type: 'quanUpdate'}) }}> + </button></td>
+//         </tr>
+//     )
+// }
+
+export default connect(getCreateStore)(Cart)
+
+// export default Cart;
--- gigarong/src/index.js
+++ gigarong/src/index.js
@@ -5,12 +5,57 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import {BrowserRouter} from 'react-router-dom';
-import {HashRouter} from 'react-router-dom';
+// import {HashRouter} from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+
+
+/**
+*  Redux쓰는 이유
+* 1. 복잡한 props 전송이 필요없음
+*    모든 컴포넌트가 직접 데이터 꺼내쓸 수 있음
+* 2. state 데이터 관리가능 (수정방법을 미리 정의)
+*/
+
+/**
+ * Redux쓰는 방법
+ * 1. <Provider>로 원하는 곳 감싸기
+ * 2. createStore에 담아서 넘기기
+ */
+
+let tempData = [ 
+  { id: 'balck', name: '테스트', quan: 2},
+  { id: 'white', name: '테스트', quan: 6},
+  { id: 'green', name: '테스트', quan: 1},
+  { id: 'blue', name: '테스트', quan: 9},
+  { id: 'yellow', name: '테스트', quan: 122},
+  { id: 'pink', name: '테스트', quan: 53}
+];
+
+let store = createStore(reducer);
+
+// state = 초기값
+function reducer(state = tempData, action ) {
+  if (action.type === 'quanAdd') {
+    let copyData = [...state]
+    copyData[0].quan++
+    return copyData
+  } else if (action.type === 'quanMin') {
+    let copyData = [...state]
+    copyData[0].quan--
+    return copyData
+  } else {
+    return state
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
