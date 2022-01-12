da9995a6a770d243df6c934130f7d72c8d791a72
poiupoiu96 <poiupoiu96@naver.com>
2022-01-12 18:58:24 +0900

    chat 왜안먹힐까..

--- gigarong/src/Chat.js
+++ gigarong/src/Chat.js
@@ -1,1 +1,30 @@
 // 1:1
+
+import React from 'react';
+import { FloatingButton, Item } from 'react-floating-button'
+import butter from './butter.svg';
+
+function chat () {
+    return (
+        <div>
+            <p> Floating Button </p>
+            <FloatingButton>
+                <Item
+                    imgSrc={butter}
+                    onClick={() => {
+                    console.log("callback function here");
+                    }}
+                />
+                <Item
+                    imgSrc={butter}
+                    onClick={() => {
+                    console.log("callback function here");
+                    }}
+                />
+            </FloatingButton>
+        </div>
+    )
+}
+
+
+export default chat 
